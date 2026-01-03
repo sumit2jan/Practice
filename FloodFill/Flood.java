f7894669cecdfdd03c3c0497ecf6d6db8634151d
sumit2jan <ss9779915666@gmail.com>
2026-01-03 21:12:37 +0530

    Practice

--- FloodFill/Flood.java
+++ FloodFill/Flood.java
@@ -21,4 +21,24 @@ class Solution {
         dfs(image, r, c + 1, originalColor, newColor); // right
         dfs(image, r, c - 1, originalColor, newColor); // left
     }
+    class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left, result);   // Left
+        result.add(node.val);         // Root
+        inorder(node.right, result);  // Right
+    }
 }
+}
+
+
+
