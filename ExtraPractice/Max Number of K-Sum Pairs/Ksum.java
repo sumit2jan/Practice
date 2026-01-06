3273c0a4f4cf51406f232376c4de4a9954a22e19
sumit2jan <ss9779915666@gmail.com>
2026-01-06 22:33:12 +0530

    practice

--- ExtraPractice/Max Number of K-Sum Pairs/Ksum.java
+++ ExtraPractice/Max Number of K-Sum Pairs/Ksum.java
@@ -28,5 +28,6 @@ class Solution {
     public static void main(String[] args){
         int[] arr = {1,2,3,4,5,6,7};
         System.out.println(maxOperations(arr,4));
+        System.out.println(maxOperations(arr,5));
     }
 }
