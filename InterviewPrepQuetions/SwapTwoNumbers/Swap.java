0285c4cf74e2c47662509a1c1c3ad6e88dec3d87
sumit2jan <ss9779915666@gmail.com>
2026-01-08 21:30:03 +0530

    feat: Done New Quetion

---
+++ InterviewPrepQuetions/SwapTwoNumbers/Swap.java
@@ -0,0 +1,15 @@
+package InterviewPrepQuetions.SwapTwoNumbers;
+
+public class Swap {
+
+public static void main(String[] args) {
+int a = 10;
+int b = 20;
+System.out.println("a is " + a + " and b is " + b);
+ a = a + b;
+ b = a - b;
+ a = a - b;
+System.out.println("After swapping, a is " + a + " and b is " + b);
+    }
+}
+
